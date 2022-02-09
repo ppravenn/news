2ecddfd930ba521413a36d2f5324d41826227bf5
ppravenn <82383651+ppravenn@users.noreply.github.com>
2022-02-09 16:33:05 +0900

    Add files via upload

---
+++ news.js
@@ -0,0 +1,40 @@
+function call(cate) {
+    var subcate = ""
+    if(cate!=='all'){
+        subcate = `&category=${cate}`
+     
+    }
+    $.ajax({
+        type: 'GET',
+        url: `https://newsapi.org/v2/top-headlines?country=kr&apiKey=cded017a336843099d467369774dd0ce${subcate}`,
+        dataType: 'json',
+        success: function (getdata) {
+            console.log(getdata)
+            usedata(getdata)
+        },
+        error: function (xhr) {
+            console.log(xhr.status + '/' + xhr.errorText)
+        }
+    })
+}
+call('all')
+function usedata(data) {
+    $('#content ul').remove()
+    var elem = `<ul>`
+    for (let i in data.articles) {
+        elem += `<li>`
+        elem += `<h2>${data.articles[i].title}</h2>`
+        elem += `<img src="${data.articles[i].urlToImage}" alt="">`
+        elem += `<p>${data.articles[i].description}</p>`
+        elem += `<div>${data.articles[i].author}</div>`
+        elem += `</li>`
+    }
+    elem += `</ul>`
+    $('#content').append(elem)
+}
+
+$('.tabtit a').on('click', function () {
+    var category = $(this).attr('href')
+        call(category)
+    return false
+})
